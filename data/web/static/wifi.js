c8a3b1a026bcef47d5303d3581219e34d0a4024d
David Borzek <mail@davidborzek.de>
2022-03-06 16:32:18 +0100

    chore: code reformatting

--- data/web/static/wifi.js
+++ data/web/static/wifi.js
@@ -1,64 +1,64 @@
 function success(image) {
-    const div = document.createElement("div");
-    div.classList.add("content")
-    div.classList.add("success")
+  const div = document.createElement("div");
+  div.classList.add("content");
+  div.classList.add("success");
 
-    div.appendChild(image);
+  div.appendChild(image);
 
-    const headline = document.createElement("h2")
-    headline.innerText = "Success";
-    div.appendChild(headline);
+  const headline = document.createElement("h2");
+  headline.innerText = "Success";
+  div.appendChild(headline);
 
-    const span = document.createElement("span");
-    span.innerText = "The Spoddify Mopped remote restarts now."
+  const span = document.createElement("span");
+  span.innerText = "The Spoddify Mopped remote restarts now.";
 
-    div.appendChild(span);
-    return div;
+  div.appendChild(span);
+  return div;
 }
 
 function formPost(path, event) {
-    const image = new Image();
-    image.className = "success";
-    image.onload = function () {
+  const image = new Image();
+  image.className = "success";
+  image.onload = function () {
+    const request = new XMLHttpRequest();
+    request.open("POST", path, true);
 
-        const request = new XMLHttpRequest();
-        request.open('POST', path, true);
+    request.onload = function () {
+      document.body.replaceChildren(success(image));
+    };
 
-        request.onload = function () {
-            document.body.replaceChildren(success(image))
-        };
+    request.onerror = function () {};
 
-        request.onerror = function () {
-        };
-
-        request.send(event ? new FormData(event.target) : undefined);
-
-    }
-    image.src = "/static/success.png";
+    request.send(event ? new FormData(event.target) : undefined);
+  };
+  image.src = "/static/success.png";
 }
 
 function updateWifi(event) {
-    event.preventDefault();
-    
-    if (!event.target[1].value && !confirm("Do you want to proceed with an empty password?")) {
-      return;
-    }
-    
-    formPost("/wifi", event)
+  event.preventDefault();
+
+  if (
+    !event.target[1].value &&
+    !confirm("Do you want to proceed with an empty password?")
+  ) {
+    return;
+  }
+
+  formPost("/wifi", event);
 }
 
 function restart() {
-    if (!confirm("Do you want to restart?")) {
-        return;
-    }
+  if (!confirm("Do you want to restart?")) {
+    return;
+  }
 
-    formPost("/restart")
+  formPost("/restart");
 }
 
 function reset() {
-    if (!confirm("This will reset all settings. Do you want to proceed?")) {
-        return;
-    }
+  if (!confirm("This will reset all settings. Do you want to proceed?")) {
+    return;
+  }
 
-    formPost("/reset")
+  formPost("/reset");
 }
